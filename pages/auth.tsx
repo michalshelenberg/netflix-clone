a80e1bdec33bcb2e8db6520d5dbff0a04bca4759
Michal Shelenberg <email@michal.sh>
2025-02-01 10:22:41 +0100

    Add authentication page and input component; update global styles

---
+++ pages/auth.tsx
@@ -0,0 +1,75 @@
+import Input from "@/components/Input";
+import { useCallback, useState } from "react";
+
+export default function Auth() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [variant, setVariant] = useState("login");
+
+  const toggleVariant = useCallback(() => {
+    setVariant((prev) => (prev === "login" ? "register" : "login"));
+  }, []);
+
+  return (
+    <div className="relative h-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <nav className="px-12 py-5">
+          <img src="/images/logo.png" alt="Logo" className="h-12" />
+          <div className="flex justify-center">
+            <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:h-2/5 lg:max-w-md rounded-md w-full">
+              <h2 className="text-white text-4xl mb-8 font-semibold">
+                {variant === "login" ? "Sign In" : "Create an account"}
+              </h2>
+              <div className="flex flex-col gap-4">
+                {variant === "register" && (
+                  <Input
+                    label="Username"
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                      setName(e.target.value)
+                    }
+                    id="name"
+                    value={name}
+                  />
+                )}
+                <Input
+                  label="Email"
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setEmail(e.target.value)
+                  }
+                  id="email"
+                  type="email"
+                  value={email}
+                />
+                <Input
+                  label="Password"
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setPassword(e.target.value)
+                  }
+                  id="password"
+                  type="password"
+                  value={password}
+                />
+              </div>
+              <button className="bg-red-600 text-white py-3 rounded-md w-full mt-10 hover:bg-red-700 transition">
+                {variant === "login" ? "Login" : "Sign Up"}
+              </button>
+              <p className="text-neutral-500 mt-12">
+                {variant === "login"
+                  ? "First time using Netflix?"
+                  : "Already have an account?"}{" "}
+                <span
+                  onClick={toggleVariant}
+                  className="text-white ml-1 hover:underline cursor-pointer"
+                >
+                  {variant === "login" ? "Create an account" : "Login"}
+                </span>
+              </p>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+}
