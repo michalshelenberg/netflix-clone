cff9967f62594cc4ac155045774dd3005f949e63
Michal Shelenberg <email@michal.sh>
2025-02-02 17:40:36 +0100

    Add GitHub and Google authentication providers; update environment variables and dependencies

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -3,6 +3,8 @@ import axios from "axios";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useCallback, useState } from "react";
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
 
 export default function Auth() {
   const router = useRouter();
@@ -92,6 +94,20 @@ export default function Auth() {
               >
                 {variant === "login" ? "Login" : "Sign Up"}
               </button>
+              <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+                <button
+                  onClick={() => signIn("google", { callbackUrl: "/" })}
+                  className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+                >
+                  <FcGoogle size={30} />
+                </button>
+                <button
+                  onClick={() => signIn("github", { callbackUrl: "/" })}
+                  className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+                >
+                  <FaGithub size={30} />
+                </button>
+              </div>
               <p className="text-neutral-500 mt-12">
                 {variant === "login"
                   ? "First time using Netflix?"
