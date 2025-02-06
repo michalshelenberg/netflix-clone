3b46dd066abddccf380e7a23d41ae88b7f2b6d08
Michal Shelenberg <email@michal.sh>
2025-02-06 08:20:24 +0100

    Add MovieList and MovieCard components; implement movie fetching with SWR and integrate into home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
 import Navbar from "@/components/Navbar";
+import useMovieList from "@/hooks/useMovieList";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 import { Inter } from "next/font/google";
@@ -24,10 +26,15 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
+  const { data: movies = [] } = useMovieList();
+
   return (
     <>
       <Navbar />
       <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+      </div>
     </>
   );
 }
